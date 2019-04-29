7403620a49524a220207df7e22d4e9da9f3657ec
Alejandro Suarez <asuarez@iri.upc.edu>
2019-04-29 18:38:08 +0200

    p02 finished

--- p02/main.py
+++ p02/main.py
@@ -3,33 +3,50 @@
 from collections import deque
 
 
-def getNumberOfDifferentPaths(graph):
-    npaths = {"Galactica": 1}
-    queue = deque(["Galactica"])
-    while q:
-        planet = queue.popleft()
+def getTopologicalOrder(graph):
+    """ Uses DFS to extract topological sort """
+    topological_order = []
+    marked = set()
+    stack = [("Galactica", False)]
+    while stack:
+        planet, visited = stack.pop()
+        if visited:
+            marked.add(planet)
+            topological_order.append(planet)
+        else:
+            stack.append((planet, True))
+            for successor in graph.get(planet, []):
+                if successor not in marked:
+                    stack.append((successor, False))
+    topological_order.reverse()
+    return topological_order
 
-        # for destination 
+
+def getNumberOfDifferentPaths(graph):
+    """ Uses Dynamic Programming to find number of different paths to each
+    node """
+    topological_order = getTopologicalOrder(graph)
+    number_of_different_paths = {planet: 0 for planet in topological_order}
+    number_of_different_paths["Galactica"] = 1
+    for planet in topological_order:
+        for successor in graph.get(planet, []):
+            number_of_different_paths[successor] += number_of_different_paths[planet]
+    return number_of_different_paths["New Earth"]
 
 
 def main():
-    q = deque(["a","b","c"])
-    print(q)
-    while q:
-        q.popleft()
-        print(q)
-    # C = int(input())
-    # for i in range(1,C+1):
-        # P = int(input())
-        # graph = {}
-        # for _ in range(P):
-            # origin, tail = input().split(":")
-            # for destination in tail.split(","):
-                # try:
-                    # graph[origin].append(destination)
-                # except KeyError:
-                    # graph[origin] = [destination]
-        # print("Case #{}: {}".format(i, getNumberOfDifferentPaths(graph)))
+    C = int(input())
+    for i in range(1,C+1):
+        P = int(input())
+        graph = {}
+        for _ in range(P):
+            origin, tail = input().split(":")
+            for destination in tail.split(","):
+                try:
+                    graph[origin].append(destination)
+                except KeyError:
+                    graph[origin] = [destination]
+        print("Case #{}: {}".format(i, getNumberOfDifferentPaths(graph)))
 
 if __name__ == "__main__":
     main()
