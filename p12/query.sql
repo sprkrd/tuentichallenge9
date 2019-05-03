bb2e2edd515a701588608e98971ddd14da90ddc1
Alejandro Suarez <asuarez@iri.upc.edu>
2019-05-03 21:13:53 +0200

    trying something, long way to go...

--- p12/query.sql
+++ p12/query.sql
@@ -1,1 +1,19 @@
-select * from activity;
+-- select time_to_sec(timediff("2018-02-21 11:03:45", "2018-02-21 11:02:15")) as diff
+
+
+select a.user_id, a.date_time as session_from, b.date_time as session_to, time_to_sec(timediff(b.date_time, a.date_time)) as seconds
+from activity a, activity b
+where a.user_id = b.user_id and a.date_time <= b.date_time and
+  !exists(select 1 from activity where user_id = a.user_id and date_time > a.date_time and date_time <= b.date_time and action = "open") and
+  !exists(select 1 from activity where user_id = a.user_id and date_time >= a.date_time and date_time < b.date_time and action = "close")
+order by a.user_id, a.date_time;
+
+
+-- select
+-- if(action = 'open', @row_number:=@row_number+1, @row_number) as num, date_time, user_id, action
+-- from
+  -- activity,(select @row_number:=0) as t
+-- -- where
+  -- -- action = "open" or action = "close"
+-- order by
+  -- user_id, date_time;
--- p12/tuenti_challenge-dump-sample.sql
+++ p12/tuenti_challenge-dump-sample.sql
@@ -26,18 +26,18 @@ CREATE TABLE `activity` (
   `date_time` datetime DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `action` varchar(30) DEFAULT NULL
-) -- ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `activity`
 --
 
--- LOCK TABLES `activity` WRITE;
+LOCK TABLES `activity` WRITE;
 /*!40000 ALTER TABLE `activity` DISABLE KEYS */;
 INSERT INTO `activity` VALUES ('2018-02-21 15:53:48',96,'close'),('2018-02-21 15:53:44',96,'action2'),('2018-02-21 16:29:22',122,'close'),('2018-02-21 15:51:51',96,'open'),('2018-02-21 15:52:57',96,'action6'),('2018-02-21 16:29:19',122,'open'),('2018-02-21 16:29:20',122,'action1'),('2018-02-21 16:50:00',122,'open'),('2018-02-21 16:50:20',122,'action1'),('2018-02-21 16:50:40',122,'action2'),('2018-02-21 08:00:00',101,'open'),('2018-02-21 08:00:30',101,'action1'),('2018-02-21 08:01:00',101,'action2'),('2018-02-21 08:02:00',101,'close'),('2018-02-21 08:15:00',101,'action1'),('2018-02-21 08:17:00',101,'action3'),('2018-02-21 09:00:00',101,'open'),('2018-02-21 09:00:28',101,'action1'),('2018-02-21 09:03:00',101,'action2'),('2018-02-21 10:00:00',101,'open'),('2018-02-21 10:00:30',101,'action1'),('2018-02-21 10:01:00',101,'action2'),('2018-02-21 10:02:00',101,'close'),('2018-02-21 11:00:00',101,'action1'),('2018-02-21 11:02:15',101,'action2'),('2018-02-21 11:03:45',101,'close'),('2018-02-21 12:00:00',101,'action1'),('2018-02-21 12:02:20',101,'action2'),('2018-02-21 11:00:00',201,'action2'),('2018-02-21 11:00:00',201,'action2'),('2018-02-21 11:00:00',202,'action4');
-/*!40000 ALTER TABLE `activity_sample` ENABLE KEYS */;
--- UNLOCK TABLES;
+/*!40000 ALTER TABLE `activity` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
