e30a4172554c3071eb32f1b9a733e59f7a628ecd
Alejandro Suarez <asuarez@iri.upc.edu>
2019-05-07 20:30:16 +0200

    solved p13 with dynamic programming

--- p13/main.py
+++ p13/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from heapq import heappush, heappop
+from itertools import combinations
 import tqdm
 
 
@@ -9,138 +10,101 @@ class Character:
         self.name = name
         self.level = level
         self.cost = cost
-        self.skills = set(skills)
-        self.reachable_skills = set(skills)
-
-
-# preprocess almanac
-
-characters = {}
-fusions = {}
-with open("almanac.data", "r") as f:
-    C, F = map(int, f.readline().split())
-    for _ in range(C):
-        line_split = f.readline().split()
-        name = line_split[0]
-        level = int(line_split[1])
-        cost = int(line_split[2])
-        skills = line_split[4:]
-        characters[name] = Character(name,level,cost,skills)
-    fusions = {char:[] for char in characters}
-    for _ in range(F):
-        result, char0, char1 = f.readline().split()
-        fusions[result].append((char0,char1))
-
-
-def relax(character):
-    relaxed = False
-    len_skills_before = len(character.reachable_skills)
-    for name0, name1 in fusions[character.name]:
-        char0 = characters[name0]
-        char1 = characters[name1]
-        if character.cost > char0.cost+char1.cost:
-            character.cost = char0.cost+char1.cost
-            relaxed = True
-        character.reachable_skills.update(char0.reachable_skills)
-        character.reachable_skills.update(char1.reachable_skills)
-    relaxed = relaxed or len(character.reachable_skills) > len_skills_before
-    return relaxed
-
-
-while True:
-    relaxed = False
-    for character in characters.values():
-        relaxed = relaxed or relax(character)
-    if not relaxed:
-        break
-
-
-class State:
-    def __init__(self, leaves, remaining_skills, cost=None):
-        self._state = (frozenset(leaves), frozenset(remaining_skills))
-        self.cost = cost or sum(characters[char].cost for char in leaves)
-
-    @property
-    def leaves(self):
-        return self._state[0]
-
-    @property
-    def remaining_skills(self):
-        return self._state[1]
-
-    def isGoal(self):
-        return not self.remaining_skills
-
-    def getSuccessors(self, budget=9999999):
-        for leaf in self.leaves:
-            for name0, name1 in fusions[leaf]:
-                root_char = characters[leaf]
-                char0 = characters[name0]
-                char1 = characters[name1]
-                cost = self.cost - root_char.cost + char0.cost + char1.cost
-                if cost > budget:
-                    continue
-                if not self.remaining_skills.intersection(char0.reachable_skills|char1.reachable_skills):
-                    continue
-                combined_skills = char0.skills | char1.skills
-                yield State(
-                        [name0,name1,*filter(lambda l: l!=leaf, self.leaves)],
-                        [s for s in self.remaining_skills if s not in combined_skills],
-                        cost)
-
-    def __eq__(self, other):
-        return self._state == other._state
-
-    def __lt__(self, other):
-        return self.cost < other.cost
-
-    def __hash__(self):
-        return hash(self._state)
+        self.skills = skills
 
     def __str__(self):
-        return "<leaves: ({}), remaining_skills: ({}), cost: {}>".format(
-                ", ".join(self.leaves),
-                ", ".join(self.remaining_skills),
-                self.cost)
+        return "<name:{},level:{},costs:{},skills:[{}]>".format(
+                self.name, self.level, self.cost, ", ".join(self.skills))
 
 
-def dijkstra(state, budget):
-    closed = set()
-    queue = [state]
-    while queue:
-        node = heappop(queue)
-        if node.isGoal():
-            return node
-        closed.add(state)
-        for successor in node.getSuccessors(budget):
-            if successor in closed:
-                continue
-            heappush(queue, successor)
-    return None
+def getAllSubsets(elements):
+    for r in range(len(elements)+1):
+        for comb in combinations(elements, r):
+            yield frozenset(comb)
+
+
+def getAllPartitions(elements, nsets):
+    elements = list(elements)
+    for i in range(nsets**len(elements)):
+        partition = [set() for _ in range(nsets)]
+        n = i
+        for j in range(len(elements)):
+            partition[n%nsets].add(elements[j])
+            n = n//nsets
+        yield [frozenset(s) for s in partition]
+
+
+def readAlmanac():
+    with open("almanac.data", "r") as f:
+        C, F = map(int, f.readline().split())
+        characters = []
+        for _ in range(C):
+            line_split = f.readline().split()
+            name = line_split[0]
+            level = int(line_split[1])
+            cost = int(line_split[2])
+            skills = frozenset(line_split[4:])
+            characters.append(Character(name, level, cost, skills))
+        fusions = {char.name: [] for char in characters}
+        for _ in range(F):
+            result, char0, char1 = f.readline().split()
+            fusions[result].append((char0,char1))
+    return characters, fusions
+
+
+characters, fusions = readAlmanac()
+char_index = {char.name: char for char in characters}
+
+
+def getReachableCharactersSortedByLevel(root_char):
+    reachable = set()
+    stack = [root_char]
+    while stack:
+        top = stack.pop()
+        if top in reachable:
+            continue
+        reachable.add(top)
+        for char0,char1 in fusions[top]:
+            stack.append(char0)
+            stack.append(char1)
+    return sorted((char_index[c] for c in reachable), key=lambda char: char.level)
+
+
+inf = 999999999
+def findMinimumGold(goal_char_name, goal_skills):
+    minimum_gold = {}
+    all_skill_subsets = list(getAllSubsets(goal_skills))
+    all_skill_partitions = list(getAllPartitions(goal_skills, 3))
+    reachable_characters = getReachableCharactersSortedByLevel(goal_char_name)
+    for char in reachable_characters:
+        for skill_subset in all_skill_subsets:
+            minimum_gold[(char.name, skill_subset)] = inf
+    for char in reachable_characters:
+        char_skill_subsets = list(getAllSubsets(char.skills&goal_skills))
+        for skill_subset in char_skill_subsets:
+            minimum_gold[(char.name, skill_subset)] = char.cost
+        for subchar_name0, subchar_name1 in fusions[char.name]:
+            subchar0 = char_index[subchar_name0]
+            subchar1 = char_index[subchar_name1]
+            for skills0, skills1, _ in all_skill_partitions:
+                cost0 = minimum_gold[(subchar_name0, skills0)]
+                cost1 = minimum_gold[(subchar_name1, skills1)]
+                for skill_subset in char_skill_subsets:
+                    combined_skills = skill_subset|skills0|skills1
+                    old_value = minimum_gold[(char.name, combined_skills)]
+                    minimum_gold[(char.name, combined_skills)] = min(old_value, cost0+cost1)
+    return minimum_gold[(goal_char_name,goal_skills)]
 
 
 def main():
-    # state = State(["phoenix"], ["dark", "ice"]) 
-    # print(state)
-    # for succ in state.getSuccessors():
-        # print("  "+str(succ))
-        # for succ_ in succ.getSuccessors():
-            # print("    "+str(succ_))
     N = int(input())
     for i in tqdm.trange(1,N+1):
         line = input().split()
         G = int(line[0])
-        name = line[1]
-        skills = [s for s in line[3:] if s not in characters[name].skills]
-        state = State([name], skills)
-        solution = dijkstra(state, G)
-        print("Case #{}: ".format(i), end="")
-        if solution is None:
-            print("IMPOSSIBLE")
-        else:
-            print(solution.cost)
-
-
+        goal_char_name = line[1]
+        goal_skills = frozenset(line[3:])
+        minimum_gold = findMinimumGold(goal_char_name, goal_skills)
+        print("Case #{}: {}".format(i, minimum_gold if minimum_gold <= G else "IMPOSSIBLE"))
 
 if __name__ == "__main__":
     main()
