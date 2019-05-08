5e2719798b67bda3497552bba03349bf913eb999
Alejandro Suarez <asuarez@iri.upc.edu>
2019-05-08 13:10:09 +0200

    cleaner code

--- p13/main.py
+++ p13/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from heapq import heappush, heappop
-from itertools import combinations
 import tqdm
 
 
@@ -15,12 +14,6 @@ class Character:
     def __str__(self):
         return "<name:{},level:{},costs:{},skills:[{}]>".format(
                 self.name, self.level, self.cost, ", ".join(self.skills))
-
-
-def getAllSubsets(elements):
-    for r in range(len(elements)+1):
-        for comb in combinations(elements, r):
-            yield frozenset(comb)
 
 
 def getAllPartitions(elements, nsets):
@@ -73,27 +66,25 @@ def getReachableCharactersSortedByLevel(root_char):
 inf = 999999999
 def findMinimumGold(goal_char_name, goal_skills):
     minimum_gold = {}
-    all_skill_subsets = list(getAllSubsets(goal_skills))
-    all_skill_partitions = list(getAllPartitions(goal_skills, 3))
+    all_skill_subsets = [p for p,_ in getAllPartitions(goal_skills, 2)]
     reachable_characters = getReachableCharactersSortedByLevel(goal_char_name)
     for char in reachable_characters:
         for skill_subset in all_skill_subsets:
             minimum_gold[(char.name, skill_subset)] = inf
-    for char in reachable_characters:
-        char_skill_subsets = list(getAllSubsets(char.skills&goal_skills))
-        for skill_subset in char_skill_subsets:
-            minimum_gold[(char.name, skill_subset)] = char.cost
+    for char in tqdm.tqdm(reachable_characters):
+        char_skills = char.skills&goal_skills
+        minimum_gold[(char.name, char_skills)] = char.cost
         for subchar_name0, subchar_name1 in fusions[char.name]:
             subchar0 = char_index[subchar_name0]
             subchar1 = char_index[subchar_name1]
-            for skills0, skills1, _ in all_skill_partitions:
-                cost0 = minimum_gold[(subchar_name0, skills0)]
-                cost1 = minimum_gold[(subchar_name1, skills1)]
-                for skill_subset in char_skill_subsets:
-                    combined_skills = skill_subset|skills0|skills1
+            for skills0 in all_skill_subsets:
+                for skills1 in all_skill_subsets:    
+                    combined_skills = char_skills|skills0|skills1
+                    cost0 = minimum_gold[(subchar_name0, skills0)]
+                    cost1 = minimum_gold[(subchar_name1, skills1)]
                     old_value = minimum_gold[(char.name, combined_skills)]
                     minimum_gold[(char.name, combined_skills)] = min(old_value, cost0+cost1)
-    return minimum_gold[(goal_char_name,goal_skills)]
+    return minimum_gold[(goal_char_name, goal_skills)]
 
 
 def main():
